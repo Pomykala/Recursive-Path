4ffd9e42f4eb7dcee69c4ad42e0731b3a2a4708f
pomykala <8mpomykala@stpatrick.org>
2017-02-17 10:48:27 -0600

    Add files via upload
    started on stroll

---
+++ path.js
@@ -0,0 +1,29 @@
+//Your code below
+function BuildPath(p,n){
+
+}
+let path = p
+let n =0
+let i =0
+let r=math.random 
+for(var n=0;i<n;n++){
+    if(r>=p)
+    return false
+    else if(true)
+}
+return path
+
+function stroll(p,l) {
+    let path=p
+    let current loction=l
+}
+//Test code
+let testpath1 = buildPath(0.10,15);
+let testpath2 = buildPath(0.50,15);
+console.log(testpath1);
+console.log(testpath2);
+console.log("---------------------");
+console.log(stroll([true,false,true]],0));
+console.log(stroll([true,true,true]],0));
+console.log(rsj(testpath1,0));
+console.log(rsj(testpath2,0));
